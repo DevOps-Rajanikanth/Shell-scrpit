35d797f881ac9066de2cce6e43ede0ad38ed8a7f
RajanikanthKore <rajanikanthkore2@gmail.com>
2023-12-13 03:06:56 +0530

    updated

--- 04-varable.sh
+++ 04-varable.sh
@@ -1,6 +1,8 @@
 PERSON1=$1
 PERSON2=$2
 
+# input parameters or input data
+
 echo "$PERSON1: Hi $PERSON2"
 echo "$PERSON2: Hi $PERSON1 , I am good , what about you"
 echo "$PERSON1: Good"
---
+++ 05-varables.sh
@@ -0,0 +1,4 @@
+USERNAME=admin
+PASSWORD=admin123
+
+echo "users name is: $USERNAME, password is $PASSWORD"
