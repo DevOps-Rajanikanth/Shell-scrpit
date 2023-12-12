8de5b28003199d05cdb58c680374080acf787274
RajanikanthKore <rajanikanthkore2@gmail.com>
2023-12-13 02:23:27 +0530

    script updated

--- 02.varables.sh
+++ 02.varables.sh
@@ -4,5 +4,5 @@ PERSON1=Ramesh
 PERSON2=Suresh
 
 echo "$PERSON1: Hi $PERSON2"
-echo "$PERSON2: Hi $PERSON1 , I'm good , what about you?"
+echo "$PERSON2: Hi $PERSON1 , I am good , what about you"
 echo "$PERSON1: "Good"
