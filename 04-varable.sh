8e8f16a5ed8987253fdd3a1651d627eeb34c5e24
RajanikanthKore <rajanikanthkore2@gmail.com>
2023-12-13 22:56:39 +0530

    modified with new code

--- 04-varable.sh
+++ 04-varable.sh
@@ -1,7 +1,7 @@
 PERSON1=$1
 PERSON2=$2
 
-# input parameters or input data
+# input parameters or input data . we could pass argumnts like Rajani , Priya
 
 echo "$PERSON1: Hi $PERSON2"
 echo "$PERSON2: Hi $PERSON1 , I am good , what about you"
