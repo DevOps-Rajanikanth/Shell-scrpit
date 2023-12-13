7fa02bae79d426897956410f43ae541719d846c8
RajanikanthKore <rajanikanthkore2@gmail.com>
2023-12-13 11:03:37 +0530

    updated

--- 05-varables.sh
+++ 05-varables.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 echo "Please enter your username:: "
-
 read -s USERNAME #the value entered above will be automatically atteched to USERNAME variable
 
 echo "Please enter your Password:: "
