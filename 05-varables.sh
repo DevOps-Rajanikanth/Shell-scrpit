bd4e3ff8e0ae8b5965e9c1a8b7b98b388ee7a54d
RajanikanthKore <rajanikanthkore2@gmail.com>
2023-12-13 10:40:49 +0530

    updated

--- 05-varables.sh
+++ 05-varables.sh
@@ -4,5 +4,11 @@ echo "please enter the username"
 
 read USERNAME
 
+echo "Please enter password"
+
+echo PASSWORD
+
 
 echo read "users name is: $USERNAME"
+
+echo read "user passwd is : $PASSWORD"
