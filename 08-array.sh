fa849e4e8af9683c24c890d0480e32065bc7c5ae
RajanikanthKore <rajanikanthkore2@gmail.com>
2023-12-14 11:38:59 +0530

    updated

--- 08-array.sh
+++ 08-array.sh
@@ -3,5 +3,5 @@
 LANGUGAGES=("java" "python")
 
 echo "Most use languages is  : ${LANGUGAGES[0]}"
-echo "All languages : ${LANGUGAGES[1}}"
-echo "All Fruits: ${LANGUGAGES[@]}"
+echo "All languages : ${LANGUGAGES[1]}"
+echo "All : ${LANGUGAGES[@]}"
