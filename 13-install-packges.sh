c77f1cd9c79780682e70ef4142aaef4bb91f30ec
RajanikanthKore <rajanikanthkore2@gmail.com>
2023-12-16 00:43:45 +0530

    Code updated

--- 13-install-packges.sh
+++ 13-install-packges.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+TIMESTAMP=$(date +%F-%S-%M-%H)
+LOGFILE="/tmp/$0-TIMESTAMP.log"
+
 ID=$(id -u)
+
 if [ $ID -ne 0]
   then 
-      echo "ERROR:"
+      echo -e "ERROR:$R Root user access denied $N"
+else
+      echo -e "You are the $Y root user $N"
+
