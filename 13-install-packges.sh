0939e615858b304e3f501ce5d1c15d793889bd96
RajanikanthKore <rajanikanthkore2@gmail.com>
2023-12-20 11:27:09 +0530

    shellscript code updated

--- 13-install-packges.sh
+++ 13-install-packges.sh
@@ -1,40 +1,45 @@
 #!/bin/bash
 
+ID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-TIMESTAMP=$(date +%F-%S-%M-%H)
-LOGFILE="/tmp/$0-TIMESTAMP.log"
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
-ID=$(id -u)
-
-if [ $ID -ne 0 ]
-  then 
-      echo -e "ERROR:$R Root user access denied $N"
-else
-      echo -e "You are the $Y root user $N"
-fi
+echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
 
 VALIDATE(){
     if [ $1 -ne 0 ]
-      then 
-          echo "ERROR: $2 ...failed!!"
+    then
+        echo -e "$2 ... $R FAILED $N"
     else
-          echo "$2 ....successed!!"
+        echo -e "$2 ... $G SUCCESS $N"
     fi
 }
 
-for packages in $@
-do
-yum list installed $packages &>> $LOGFILE
-if [ $? -ne 0 ]
-  then
-      yum install $packages &>> $LOGFILE
-      VALIDATE $? "Installtion of $packages"
+if [ $ID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1 # you can give other than 0
 else
-      echo -e "$packages already installed.... $Y skipping $N"
-fi
-done
+    echo "You are root user"
+fi # fi means reverse of if, indicating condition end
 
+#echo "All arguments passed: $@"
+# git mysql postfix net-tools
+# package=git for first time
+
+for package in $@
+do
+    yum list installed $package &>> $LOGFILE #check installed or not
+    if [ $? -ne 0 ] #if not installed
+    then
+        yum install $package -y &>> $LOGFILE # install the package
+        VALIDATE $? "Installation of $package" # validate
+    else
+        echo -e "$package is already installed ... $Y SKIPPING $N"
+    fi
+done
